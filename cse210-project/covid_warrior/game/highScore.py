51a00ba463e34a4006b579516ebd28535e8a53e1
McKay Williams <wmsmckay@McKays-MacBook-Air.local>
2021-03-27 20:50:01 -0600

    fixed file not found error

--- cse210-project/covid_warrior/game/highScore.py
+++ cse210-project/covid_warrior/game/highScore.py
@@ -7,7 +7,7 @@ class HighScore():
     This class initializes the high score. Gets the value from text file.
     """
     def __init__(self): 
-        self.scoreFile = open(os.path.join(constants.PATH, "highScore\highScore.txt"), "r")
+        self.scoreFile = open(os.path.join(constants.PATH, "./highScore/highScore.txt"), "r")
         self.highScoreRead = self.scoreFile.read()
         self.scoreFile.close()
         
@@ -16,7 +16,7 @@ class HighScore():
         Changes the high score if reached
         """
         self.highScore = str(score)
-        self.scoreFile = open(os.path.join(constants.PATH, "./highScore\highScore.txt"), "w")
+        self.scoreFile = open(os.path.join(constants.PATH, "./highScore/highScore.txt"), "w")
         #self.scoreFile = open("highScore/highScore.txt", "w")
         self.scoreFile.write(self.highScore)
         self.scoreFile.close()
